7eb75b89767b66c44df5d027655e852ab48006dc
Marc Clascà <hulehule20@gmail.com>
2016-12-03 01:19:17 +0100

    Fixed tests

--- test/spec/controllers/profile.js
+++ test/spec/controllers/profile.js
@@ -17,7 +17,4 @@ describe('Controller: ProfileCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(ProfileCtrl.awesomeThings.length).toBe(3);
-  });
 });
